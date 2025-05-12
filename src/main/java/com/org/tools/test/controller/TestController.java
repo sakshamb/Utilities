ca351fe062af4f9e9d01cf7ca500448e17ab7b92
Saksham <batra.saksham@gmail.com>
2025-05-12 13:34:39 +0530

    improvements to scheduler

--- src/main/java/com/org/tools/test/controller/TestController.java
+++ src/main/java/com/org/tools/test/controller/TestController.java
@@ -1,62 +1,28 @@
-package com.bosch.pssb.test.controller;
+package com.org.tools.test.controller;
 
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.ZoneId;
-import java.time.temporal.ChronoUnit;
-import java.util.Date;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.scheduling.TaskScheduler;
-import org.springframework.scheduling.concurrent.ConcurrentTaskScheduler;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
-import org.springframework.scheduling.config.TaskSchedulerRouter;
-import org.springframework.scheduling.support.CronTrigger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.org.tools.test.utils.SchedulerUtils;
+
 @Controller
 public class TestController {
 
+	@Autowired
+	SchedulerUtils schedulerUtils;
+	
 	@GetMapping("/singleInstanceScheduler")
 	@ResponseBody
 	public String createSingleTriggerScheduler() {
-		Instant instant=Instant.now();
-		instant=instant.atZone(ZoneId.systemDefault()).with(LocalTime.parse("16:22:00")).toInstant();
-		System.out.println(instant.plus(2, ChronoUnit.MINUTES ));
-		ThreadPoolTaskScheduler scheduler= new ThreadPoolTaskScheduler();
-		scheduler.setPoolSize(1);
-		scheduler.initialize();
-		
-		Runnable taskTest= new Runnable() {
-			public void run() {
-				System.out.println("scheduler running at :" + Instant.now());
-			}
-		};
-		
-		scheduler.schedule(taskTest, instant.plus(2, ChronoUnit.MINUTES ));
-		return "ok";
+		return schedulerUtils.createSingleTriggerScheduler();
 	}
 	
 	@GetMapping("/multiInstanceScheduler")
 	@ResponseBody
 	public String createMultiTriggerScheduler() {
-		// scheduling every min for quick testing
-		CronTrigger trigger=new CronTrigger("0 0/1 * * * ? ");
-		ThreadPoolTaskScheduler scheduler= new ThreadPoolTaskScheduler();
-		scheduler.setPoolSize(1);
-		scheduler.initialize();
-		
-		Runnable taskTest= new Runnable() {
-			public void run() {
-				System.out.println("scheduler running at :" + Instant.now());
-			}
-		};
-		
-		scheduler.schedule(taskTest, trigger);	
-		return "ok";
+		return schedulerUtils.createMultiTriggerScheduler();
 	}
-	
 }
+	
